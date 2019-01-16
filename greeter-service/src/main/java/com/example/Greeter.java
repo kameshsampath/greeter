35f9b2ec5e7de5068c850242c19d5c8d0a2d853b
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-01-16 08:01:11 +0530

    Knative check

--- greeter-service/src/main/java/com/example/Greeter.java
+++ greeter-service/src/main/java/com/example/Greeter.java
@@ -6,11 +6,11 @@ public class Greeter {
 
     private final String FORMAT = "Hello %s.";
 
-    public String sayHello(String name, Context context) {
+    public String sayHello(Object input, Context context) {
         if (context != null) {
             // do something with context
             // context will not be null only when deployed in AWS Lambda
         }
-        return String.format(FORMAT, name);
+        return String.format(FORMAT, input);
     }
 }
